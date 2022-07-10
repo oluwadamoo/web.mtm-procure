4a0c3a0ef2d9792828c070344a441573e98420fc
oluwadamoo <mrdamilolasaliu@gmail.com>
2022-07-10 15:46:44 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React, { lazy } from 'react'
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
-import AccessibleNavigationAnnouncer from './components/AccessibleNavigationAnnouncer'
+import React, { lazy } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import AccessibleNavigationAnnouncer from "./components/AccessibleNavigationAnnouncer";
 
-const Layout = lazy(() => import('./containers/Layout'))
-const Login = lazy(() => import('./pages/Login'))
-const CreateAccount = lazy(() => import('./pages/CreateAccount'))
-const ForgotPassword = lazy(() => import('./pages/ForgotPassword'))
+const Layout = lazy(() => import("./containers/Layout"));
+const Login = lazy(() => import("./pages/Login"));
+const CreateAccount = lazy(() => import("./pages/CreateAccount"));
+const ForgotPassword = lazy(() => import("./pages/ForgotPassword"));
 
 function App() {
   return (
@@ -13,9 +18,9 @@ function App() {
       <Router>
         <AccessibleNavigationAnnouncer />
         <Switch>
-          <Route path="/login" component={Login} />
+          {/* <Route path="/login" component={Login} />
           <Route path="/create-account" component={CreateAccount} />
-          <Route path="/forgot-password" component={ForgotPassword} />
+          <Route path="/forgot-password" component={ForgotPassword} /> */}
 
           {/* Place new routes over this */}
           <Route path="/app" component={Layout} />
@@ -24,7 +29,7 @@ function App() {
         </Switch>
       </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
